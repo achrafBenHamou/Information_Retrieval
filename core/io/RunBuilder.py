9173a0c4927468d983eb6454ab4e311e0a8197c9
AbouOpenSource <aboudra1996@gmail.com>
2020-12-21 16:13:48 +0100

    finalize the RunBuilder class; Now the runs is well done !! :-)

--- core/io/RunBuilder.py
+++ core/io/RunBuilder.py
@@ -4,38 +4,35 @@ from config.Config import ConfigFile
 class RunBuilder:
 
     def __init__(self,
-                results,
-                group="AbouAchrafAmineAli",
-                step=ConfigFile().get_run_config("step"),
-                w_func=ConfigFile().get_run_config("w_func"),
-                step_min=ConfigFile().get_run_config("step_min"),
-                search_level=ConfigFile().get_run_config("search_level")
-                ):
+                 results,
+                 group="AbouAchrafAmineAli",
+                 step=ConfigFile().get_run_config("step"),
+                 w_func=ConfigFile().get_run_config("w_func"),
+                 step_min=ConfigFile().get_run_config("step_min"),
+                 search_level=ConfigFile().get_run_config("search_level")
+                 ):
         self.results = results
         self.group = group
         self.step = step
         self.step_min = step_min
         self.w_func = w_func
         self.search_level = search_level
-        self.display()
+        self.make_run()
 
-    def display(self):
-        for query in self.results:
-            temp = dict(sorted(self.results[query].items(), key=lambda item: item[1], reverse=True))
-            for item in temp:
-
-            print(temp)
-
-    def build_run(self, results):
-        path = ConfigFile().get_data_config("run_folder")
+    def make_run(self, path=ConfigFile().get_data_config("run_folder")):
         output_file = open(path +
                            self.group +
-                           "_" + self.step +
-                           "_" + self.step_min +
+                           "_" + str(self.step) +
+                           "_" + str(self.step_min) +
                            "_" + self.w_func +
                            "_" + self.search_level +
-                           ".txt", "a")
-
-    def write_in_file(self, results, output_file, req, count):
-        output_file.write("{} {} {} {} {} {} {}\n"
-                          .format("req", "Q0", "max_doc[0]", "i", "max_doc[1]", self.group, "/article[1]"))
+                           ".txt", "w")
+        for query in self.results:
+            temp = dict(sorted(self.results[query].items(), key=lambda item: item[1], reverse=True))
+            count = 1
+            for doc_id in temp:
+                output_file.write("{} {} {} {} {} {} {}\n"
+                                  .format(query.strip(), "Q0", doc_id.strip(), count, temp[doc_id], self.group, "/article[1]"))
+                count = count + 1
+                if count == 1501:
+                    break
